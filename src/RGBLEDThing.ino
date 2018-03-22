e954bd8fabaf6bb635fab546a5972665b35e22cc
Gerben <gerbenvaneerten@gmail.com>
2018-03-22 16:35:04 +0100

    Switched to ootb namespace

--- src/RGBLEDThing.ino
+++ src/RGBLEDThing.ino
@@ -6,7 +6,7 @@
 #define PIN D2
 #define BUFFER_SIZE 1024
 
-using namespace g3rb3n;
+using namespace ootb;
 
 Thing thing;
 BlinkPattern led(BUILTIN_LED);
@@ -21,51 +21,51 @@ bool state = false;
 
 void setup()
 {
-  Serial.begin(230400);
-  Serial.println();
-  Serial.println("Client:" + thing.clientId());
+    Serial.begin(230400);
+    Serial.println();
+    Serial.println("Client:" + thing.clientId());
 
-  led.setPattern(start);
+    led.setPattern(start);
 
-  pixels.begin();
+    pixels.begin();
 
-  pixels.setPixelColor(0, pixels.Color(10, 0, 0));
-  pixels.show();
-
-  thing.onStateChange([](const String& msg){
-    Serial.println(msg);
-  });
-
-  thing.begin();
-
-  thing.addActuator(thing.clientId() + "/rgbled/color", [](Value& value){
-    state = (bool)value;
-    StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
-    String msg = value;
-    JsonObject& root = jsonBuffer.parseObject(msg);
-
-    if (!root.success()) {
-      Serial.println("parseObject() failed");
-      return false;
-    }
-
-    int r = root["r"];
-    int g = root["g"];
-    int b = root["b"];
-    Serial.println(r);
-    Serial.println(g);
-    Serial.println(b);
-    pixels.setPixelColor(0, pixels.Color(r, g, b));
+    pixels.setPixelColor(0, pixels.Color(10, 0, 0));
     pixels.show();
-    Serial.println("Got:" + String(value));
-    //thing.publish(thing.clientId() + "/rbgled/color", value);
-  });
 
-  led.setPattern(normal);
+    thing.onStateChange([](const String& msg){
+        Serial.println(msg);
+    });
+
+    thing.begin();
+
+    thing.addActuator("rgbled/color/" + thing.clientId(), [](Value& value){
+        state = (bool)value;
+        StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+        String msg = value;
+        JsonObject& root = jsonBuffer.parseObject(msg);
+
+        if (!root.success()) {
+        Serial.println("parseObject() failed");
+        return false;
+        }
+
+        int r = root["r"];
+        int g = root["g"];
+        int b = root["b"];
+        Serial.println(r);
+        Serial.println(g);
+        Serial.println(b);
+        pixels.setPixelColor(0, pixels.Color(r, g, b));
+        pixels.show();
+        Serial.println("Got:" + String(value));
+        //thing.publish(thing.clientId() + "/rbgled/color", value);
+    });
+
+    led.setPattern(normal);
 }
 
 void loop()
 {
-  thing.handle();
-  led.handle();
+    thing.handle();
+    led.handle();
 }
