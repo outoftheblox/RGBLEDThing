d52a62db2973f0ce3c108a30783f3d1aec516262
Gerben <info@gerbenvaneerten.nl>
2023-11-26 18:09:02 +0100

    Update

--- src/RGBLEDThing.ino
+++ src/RGBLEDThing.ino
@@ -3,7 +3,14 @@
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
 
+#ifdef BOARD_7_LEDS
+#define PIN D4
+#define NUM_LEDS 7
+#else
 #define PIN D2
+#define NUM_LEDS 1
+#endif
+
 #define BUFFER_SIZE 1024
 
 using namespace ootb;
@@ -15,7 +22,7 @@ BlinkPattern::Pattern<2> start{{1,9},25};
 BlinkPattern::Pattern<2> normal{{1,39},25};
 BlinkPattern::Pattern<0> disable{{},1000};
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 bool state = false;
 
@@ -38,7 +45,8 @@ void setup()
 
     thing.begin();
 
-    thing.addActuator(thing.clientId() + "/rgbled/color", [](Value& value){
+    String topic = "things/" + thing.clientId() + "/rgbled/color";
+    thing.addActuator(topic, [](Value& value){
         state = (bool)value;
         StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
         String msg = value;
@@ -55,10 +63,15 @@ void setup()
         Serial.println(r);
         Serial.println(g);
         Serial.println(b);
-        pixels.setPixelColor(0, pixels.Color(r, g, b));
-        pixels.show();
+        for (int i = 0 ; i < NUM_LEDS ; ++i)
+        {
+            pixels.setPixelColor(i, pixels.Color(r, g, b));
+            pixels.show();
+            delay(50);
+        }
         Serial.println("Got:" + String(value));
         //thing.publish(thing.clientId() + "/rbgled/color", value);
+        return true;
     });
 
     led.setPattern(normal);
